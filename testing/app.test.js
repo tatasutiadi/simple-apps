79091f82b60c4e8104b7b404539a678f5a741a5a
Tata Sutiadi <tatasutiadi@gmail.com>
2023-05-30 10:03:31 +0700

    update test

--- testing/app.test.js
+++ testing/app.test.js
@@ -13,14 +13,14 @@ describe('Unit Test /', () => {
 
 describe('Unit Test /app1', () => {
     it('should respond with "Hello App1!"', async () => {
-      const response = await request(app).get('/app1');
+      const response = await request(app).get('/app');
       expect(response.status).toBe(200);
     });
   });
 
   describe('Unit Test /app2', () => {
     it('should respond with "Hello App2!"', async () => {
-      const response = await request(app).get('/app2');
+      const response = await request(app).get('/test');
       expect(response.status).toBe(200);
     });
   });
@@ -35,7 +35,7 @@ describe('Integration Test Connect Database', () => {
       host: 'localhost',
       user: 'peserta',
       password: 'password',
-      database: 'training'
+      database: 'devops'
     });
 
     // Set the connection for the application to use
