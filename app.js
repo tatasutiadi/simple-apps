4ca4dac3ed57bca6602b85e0574fc9db551985de
Tata Sutiadi <tatasutiadi@gmail.com>
2023-05-30 13:20:03 +0700

    update code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const mysql = require('mysql');
 const app = express()
-const PORT = 3000;
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 const path = require('path')
+
+app.disable("x-powered-by");
 
 // Import Middleware
 const logger = require('./middleware/logger')
