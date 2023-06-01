2475d822b85ff641dc1863373a57b5b102ab1aa5
Tata Sutiadi <tatasutiadi@gmail.com>
2023-06-01 11:06:36 +0700

    update source

--- apps/testing/app.test.js
+++ apps/testing/app.test.js
@@ -39,7 +39,7 @@ describe('Integration Test Connect Database', () => {
     // Create a connection pool instead of a single connection
     connection = mysql.createPool({
       connectionLimit: 10, // Adjust the limit according to your requirements
-      host: 'localhost',
+      host: 'db',
       user: 'peserta',
       password: 'password',
       database: 'training'
